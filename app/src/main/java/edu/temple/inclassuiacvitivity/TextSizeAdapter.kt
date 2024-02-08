4b7ff9b3de044b49979f68ffe2d59d6be2565cc6
Jason Hankins <tul53850@temple.edu>
2024-02-08 11:41:10 -0500

    Implemented getItemId() + initial getView()

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class TextSizeAdapter(_context: Context, resource: Int, _numbers: Array<String>) : BaseAdapter() {
 
@@ -19,11 +20,16 @@ class TextSizeAdapter(_context: Context, resource: Int, _numbers: Array<String>)
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = numbers[position]
+        textView.textSize = numbers[position].toFloat()
+        textView.setPadding(5,10,0,10)
+        return textView
     }
 
 }
